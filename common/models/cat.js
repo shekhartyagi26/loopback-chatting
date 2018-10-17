6f5cb6342d38306929f62eb0c7004794c5a7e31c
fluper36 <shadabsaifi03@gmail.com>
2018-10-17 19:34:49 +0530

    Readme.md

--- common/models/cat.js
+++ common/models/cat.js
@@ -1,60 +1,60 @@
 'use strict';
-var pubsub = require('../../server/pubsub')
+// var pubsub = require('../../server/pubsub')
 var loopback = require('loopback');
 
 module.exports = function(Cat) {
 
     // Cat after save..
-  Cat.observe('after save', function (ctx, next) {
-    var socket = Cat.app.io;
-    if (ctx.isNewInstance) {
-      //Now publishing the data..
-      pubsub.publish(socket, {
-        collectionName: 'Cat',
-        data: ctx.instance,
-        method: 'POST'
-      });
-    } 
-    else {
-      //Now publishing the data..
-      pubsub.publish(socket, {
-        collectionName: 'Cat',
-        data: ctx.instance,
-        modelId: ctx.instance.id,
-        method: 'PUT'
-      });
-    }
-    //Calling the next middleware..
-    next();
-  }); //after save..
-  //CatDetail before delete..
-  Cat.observe("before delete", function (ctx, next) {
-    var socket = Cat.app.io;
-    //Now publishing the data..
-    pubsub.publish(socket, {
-      collectionName: 'Cat',
-      data: ctx.instance.id,
-      modelId: ctx.instance.id,
-      method: 'DELETE'
-    });
-    //move to next middleware..
-    next();
-  }); //before delete..
+  // Cat.observe('after save', function (ctx, next) {
+  //   var socket = Cat.app.io;
+  //   if (ctx.isNewInstance) {
+  //     //Now publishing the data..
+  //     pubsub.publish(socket, {
+  //       collectionName: 'Cat',
+  //       data: ctx.instance,
+  //       method: 'POST'
+  //     });
+  //   } 
+  //   else {
+  //     //Now publishing the data..
+  //     pubsub.publish(socket, {
+  //       collectionName: 'Cat',
+  //       data: ctx.instance,
+  //       modelId: ctx.instance.id,
+  //       method: 'PUT'
+  //     });
+  //   }
+  //   //Calling the next middleware..
+  //   next();
+  // }); //after save..
+  // //CatDetail before delete..
+  // Cat.observe("before delete", function (ctx, next) {
+  //   var socket = Cat.app.io;
+  //   //Now publishing the data..
+  //   pubsub.publish(socket, {
+  //     collectionName: 'Cat',
+  //     data: ctx.instance.id,
+  //     modelId: ctx.instance.id,
+  //     method: 'DELETE'
+  //   });
+  //   //move to next middleware..
+  //   next();
+  // }); //before delete..
 
 
-  Cat.greet = function (msg, cb) {
-    cb(null, 'Greetings... ' + msg);
-  }
+  // Cat.greet = function (msg, cb) {
+  //   cb(null, 'Greetings... ' + msg);
+  // }
 
-  Cat.remoteMethod('greet', {
-    accepts: {
-      arg: 'msg',
-      type: 'string'
-    },
-    returns: {
-      arg: 'greeting',
-      type: 'string'
-    }
-  });
+  // Cat.remoteMethod('greet', {
+  //   accepts: {
+  //     arg: 'msg',
+  //     type: 'string'
+  //   },
+  //   returns: {
+  //     arg: 'greeting',
+  //     type: 'string'
+  //   }
+  // });
 
 };
--- server/server.js
+++ server/server.js
@@ -2,19 +2,22 @@
 
 var loopback = require('loopback');
 var boot = require('loopback-boot');
-
+const mongoose = require('mongoose')
 var app = module.exports = loopback();
 
 app.start = function () {
   // start the web server
   return app.listen(function () {
     app.emit('started');
+    console.log(' Started .');
+    
     var baseUrl = app.get('url').replace(/\/$/, '');
     console.log('Web server listening at: %s', baseUrl);
     if (app.get('loopback-component-explorer')) {
       var explorerPath = app.get('loopback-component-explorer').mountPath;
       console.log('Browse your REST API at %s%s', baseUrl, explorerPath);
     }
+
   });
 };
 
@@ -22,16 +25,15 @@ boot(app, __dirname, function (err) {
 
   if (err) throw err;
   // start the server if `$ node server.js`
-  if (require.main === module) {
-    app.io = require('socket.io')(app.start);
-    app.io.on('connection', function (socket) {
 
+  if (require.main === module) {
+    let io = require('socket.io')(app.start());
+    io.on('connection', function (socket) {
+      console.log("user connected")
       var myModelName = app.models.cat;
       let options = {
         limit: 100,
-        sort: {
-          _id: 1
-        }
+        sort: {_id: 1}
       }
       
       myModelName.find({}, options, (err, res) => {
@@ -40,20 +42,30 @@ boot(app, __dirname, function (err) {
         else {
           socket.emit('output', res)
         }
-
       })
 
       socket.on('input', function (message) {
-        myModelName.insert({
-          message: message
-        }, () => {
-          app.io.emit('output', [data])
+        let data = { message: message }
+        new myModelName(data).save((err, result)=>{
+          if(err)
+            throw err
+          else
+            {
+              console.log("result====>>>>",result)
+              myModelName.findOne({_id:mongoose.Types.ObjectId(result.id)},(err, res) => {
+                console.log("new id==>>>",res)
+                if (err)
+                  throw err
+                else {
+                  socket.emit('output', res)
+                }
+              })
+            }
         })
-
       });
 
       socket.on('disconnect', function () {
-
+        console.log("user disconected")
       });
 
     });
