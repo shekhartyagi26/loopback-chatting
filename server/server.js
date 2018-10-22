8da059faba414dfff0d342b1b62f900aceb04f88
fluper36 <shadabsaifi03@gmail.com>
2018-10-22 12:10:39 +0530

    Room Create

--- server/server.js
+++ server/server.js
@@ -2,7 +2,6 @@
 
 var loopback = require('loopback');
 var boot = require('loopback-boot');
-const mongoose = require('mongoose')
 var app = module.exports = loopback();
 
 app.start = function () {
@@ -10,7 +9,6 @@ app.start = function () {
   return app.listen(function () {
     app.emit('started');
     console.log(' Started .');
-    
     var baseUrl = app.get('url').replace(/\/$/, '');
     console.log('Web server listening at: %s', baseUrl);
     if (app.get('loopback-component-explorer')) {
@@ -22,50 +20,32 @@ app.start = function () {
 };
 
 boot(app, __dirname, function (err) {
-
   if (err) throw err;
-  // start the server if `$ node server.js`
-
   if (require.main === module) {
     let io = require('socket.io')(app.start());
     io.on('connection', function (socket) {
       var myModelName = app.models.cat;
-      let options = {
-        limit: 100,
-        sort: {_id: 1}
-      }
-      
-      myModelName.find({}, options, (err, res) => {
+      myModelName.find((err, res) => {
         if (err)
           throw err
-        else {
+        else
           socket.emit('output', res)
-        }
       })
-
-      socket.on('input', function (message) {
-        let data = { message: message }
-        new myModelName(data).save((err, result)=>{
-          if(err)
-            throw err
-          else
-            {
-              console.log(result)
-              myModelName.findById(mongoose.Types.ObjectId(result.id),(err, res) => {
-                if (err)
-                  throw err
-                else {
-                  io.emit('output', res)
-                }
-              })
-            }
-        })
+      socket.on('room', function (data) {
+        socket.join(data.roomId);
+        socket.on('input', function (data) {
+          data.socketId = socket.id
+          new myModelName(data).save((err, result) => {
+            if (err)
+              throw err
+            else
+              io.emit('output', result)
+          })
+        });
       });
-
       socket.on('disconnect', function () {
-        
-      });
 
+      });
     });
   }
 });
